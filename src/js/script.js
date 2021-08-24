3050d72fd60d7fcea863f49fd7759f73bda9f2b7
Oleg <youoleg212@gmail.com>
2021-08-24 23:24:24 +0300

    add php file

--- src/js/script.js
+++ src/js/script.js
@@ -77,5 +77,22 @@ $(document).ready(function(){
     valideForms('#order form')
 
     $('input[name=phone]').mask("+7(999)999-99-99")
+
+    $('form').submit(function(e){
+      e.preventDefault();
+      $.ajax({
+          type:"POST",
+          url:"mailer/smart.php",
+          data:$(this).serialize()
+      }).done(function(){
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn();
+
+
+          $('form').trigger('reset');
+      });
+      return false;
+    });
 });
 
